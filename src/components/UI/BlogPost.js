65bc3e9b0032cdd24d6e090217629e944ffde2d1
nubelsondev <nubelsondev@hotmail.com>
2020-04-07 12:35:17 +0100

    make some components reusable

--- src/components/UI/BlogPost.js
+++ src/components/UI/BlogPost.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import Tags from './Tags'
+
 const Container = styled.div`
     position: relative;
     width: 32.5rem;
@@ -12,41 +14,20 @@ const Container = styled.div`
     box-shadow: 2.5px 5px 8.4px rgba(0, 0, 0, 0.25);
 `
 
-const Title = styled.h2`
-    font-size: 1.8rem;
+export const Title = styled.h2`
+    font-size: ${props => (props.size ? `${props.size}rem` : '1.8rem')};
     color: var(--text-highlight);
     font-weight: 600;
     display: inline-block;
     margin-bottom: 2rem;
 `
 
-const Info = styled.p`
-    font-size: 1.3rem;
+export const Info = styled.p`
+    font-size: ${props => (props.size ? `${props.size}rem` : '1.3rem')};
 
     span {
         font-weight: bold;
         color: var(--primary);
-    }
-`
-
-const Tags = styled.div`
-    display: flex;
-    align-items: center;
-    font-weight: 600;
-    text-transform: uppercase;
-    position: absolute;
-    bottom: 2rem;
-
-    p:not(:last-child) {
-        margin-right: 1rem;
-    }
-
-    p {
-        color: #000;
-        background-color: var(--primary);
-        padding: 0.2rem 1.2rem;
-        display: inline-block;
-        border-radius: var(--radius);
     }
 `
 
@@ -58,12 +39,7 @@ const BlogPost = ({ title, date, author, tags }) => {
                 {date}, by <span>{author}</span>
             </Info>
 
-            <Tags>
-                {tags.map((tag, index) => {
-                    const hashtag = `#${tag}`
-                    return <p key={index}> {hashtag} </p>
-                })}
-            </Tags>
+            <Tags card tags={tags} />
         </Container>
     )
 }
---
+++ src/components/UI/Tags.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledTags = styled.div`
+    display: flex;
+    align-items: center;
+    font-weight: 600;
+    font-size: ${props => (props.size ? `${props.size}rem` : '1rem')};
+    text-transform: uppercase;
+    position: ${props => props.card && 'absolute'};
+    bottom: 2rem;
+
+    p:not(:last-child) {
+        margin-right: 1rem;
+    }
+
+    p {
+        color: #000;
+        background-color: var(--primary);
+        padding: 0.2rem 1.2rem;
+        display: inline-block;
+        border-radius: var(--radius);
+    }
+`
+
+const Tags = ({ tags, size, card }) => {
+    return (
+        <StyledTags size={size} card={card}>
+            {tags.map((tag, index) => {
+                const hashtag = `#${tag.split(' ').join('')}`
+                return <p key={index}> {hashtag} </p>
+            })}
+        </StyledTags>
+    )
+}
+
+export default Tags
